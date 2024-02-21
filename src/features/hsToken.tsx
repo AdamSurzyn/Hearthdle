9f75dfcccccb3607f08c013255bfdb4cace58b87
AdamS <surzyn.adam@gmail.com>
2024-02-21 07:13:30 +0100

    Dev: Adding hs api token request

---
+++ src/features/hsToken.tsx
@@ -0,0 +1,15 @@
+const clientId = "";
+const clientSecret = "";
+
+const clientToken = async () => {
+  const response = await fetch("https://oauth.battle.net/token", {
+    method: "POST",
+    headers: {
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+    },
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+    }),
+  });
+  const data = await response.json();
+};
