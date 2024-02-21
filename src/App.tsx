c12cc47fcd36bf38fd25ce9f257701345a10dc95
mkokosinski <mkokosinski91@gmail.com>
2024-02-21 17:50:50 +0100

    zmiana

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+
+console.log('asdasd');
 
 function App() {
-  return <div className="App"></div>;
+  return <div className='App'></div>;
 }
 
 export default App;
