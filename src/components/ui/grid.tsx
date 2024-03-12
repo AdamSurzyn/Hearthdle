6f22ffc5ae6d96859beb0b45c211949e5c6841d7
AdamS <surzyn.adam@gmail.com>
2024-03-12 20:48:29 +0100

    Dev: Adding styling to cards grid

--- src/components/ui/grid.tsx
+++ src/components/ui/grid.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import "../ui/grid.scss";
 const Grid = () => {
-  return <div>Grid Here</div>;
+  return (
+    <div className="cards-grid-container">
+      <div className="card">Example1</div>
+      <div className="card">Example2</div>
+      <div className="card">Example3</div>
+    </div>
+  );
 };
 
 export default Grid;
