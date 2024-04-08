743a9798e7d791ee10189e57a512020ae9c77bae
AdamSurzyn <surzyn.adam@gmail.com>
2024-04-08 22:12:56 +0200

    Dev: Adding the search feature. Still having a type error with scroll component

---
+++ src/components/ui/search/scroll.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Scroll = (children : React.ReactNode) =>{
+    return (
+        <div className="scroll">{children}</div>
+    )
+}
+
+export default Scroll 
--- src/components/ui/search/search.tsx
+++ src/components/ui/search/search.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../ui/search/search.scss";
 import { useCardsQuery } from "../../hooks/cardsQuery";
+import { CardCommonAttributes } from "./searchTypes";
+
+
 const futureSearch = () => {};
 
 const Search = () => {
+  const [searchField, setSearchField] = useState("");
   const cards = useCardsQuery();
+  const filteredCards = cards.filter((card: CardCommonAttributes) =>{card.name.toLowerCase().includes(searchField)})
+
+
   console.log(cards);
   return (
     <div className="card-search-container">
