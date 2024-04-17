922b73a19f6202735f741ebd349a1e21b03e506d
AdamS <surzyn.adam@gmail.com>
2024-04-17 21:18:11 +0200

    Chore: Removing console.log message

--- src/features/getCards.tsx
+++ src/features/getCards.tsx
@@ -3,7 +3,6 @@ import clientToken from "./hsToken";
 export const allCards = async () => {
   try {
     const access_token = await clientToken();
-    console.log(access_token);
     const response = await fetch(
       "https://eu.api.blizzard.com/hearthstone/cards?locale=en_US&rarity=legendary&pageSize=850",
       {
