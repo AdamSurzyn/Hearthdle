f9740484b0306bc333f66f271a90a915c3ff979f
AdamS <surzyn.adam@gmail.com>
2024-05-07 21:09:50 +0200

    Chore: Updating main page imports, adding context

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,12 +1,14 @@
 import Search from "../components/ui/search/search";
 import Grid from "../components/ui/grid";
 import "../pages/game.scss";
-import { CardsQueryData } from "../components/ui/search/searchTypes";
+import { CardsQueryData } from "../types/searchTypes";
 import { useQuery } from "react-query";
 import { allCards } from "../features/getCards";
 import { randomCard } from "../utils/randomCard";
+import { useSearchCardContext } from "../contexts/CardsContext";
 
 const Game = () => {
+  const currentSearchCard = useSearchCardContext();
   const {
     error,
     data: cards,
