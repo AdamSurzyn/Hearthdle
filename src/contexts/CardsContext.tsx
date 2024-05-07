6252b04c525ed6087fb20bd494dad23872ec83f9
AdamS <surzyn.adam@gmail.com>
2024-05-07 21:08:06 +0200

    Dev: Adding untested context for search card

---
+++ src/contexts/CardsContext.tsx
@@ -0,0 +1,40 @@
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+import { CardCommonAttributes } from "../types/searchTypes";
+
+type ContextSearchCard = {
+  searchCard: CardCommonAttributes | null;
+  setSearchCard: Dispatch<SetStateAction<CardCommonAttributes | null>>;
+};
+
+const SearchCardContext = createContext<ContextSearchCard | null>(null);
+export default SearchCardContext;
+
+export const SearchCardProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [searchCard, setSearchCard] = useState<CardCommonAttributes | null>(
+    null
+  );
+
+  return (
+    <SearchCardContext.Provider value={{ searchCard, setSearchCard }}>
+      {children}
+    </SearchCardContext.Provider>
+  );
+};
+
+export const useSearchCardContext = () => {
+  const ctx = useContext(SearchCardContext);
+
+  if (!ctx) {
+    throw Error("Missing SearchCardContext, it's not wrapped in the provider");
+  }
+};
