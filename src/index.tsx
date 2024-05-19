1d2ffb7b7f27bfe65894a705ce6f15f5a15980f0
AdamS <surzyn.adam@gmail.com>
2024-05-19 18:30:46 +0200

    Chore: Removing reduntant import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Game from "./pages/game";
 import { QueryClient, QueryClientProvider } from "react-query";
