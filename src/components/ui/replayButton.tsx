ba684a375861d2b7bba1282e0f1a606468f1da0b
AdamS <surzyn.adam@gmail.com>
2024-09-11 19:17:51 +0200

    Chore: styling replay button.

--- src/components/ui/replayButton.tsx
+++ src/components/ui/replayButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./replayButton.scss";
 // Define the props type for the component
 interface ReplayButtonProps {
   onReset: () => void;
@@ -8,5 +8,11 @@ interface ReplayButtonProps {
 // Create the functional component with props type
 export const ReplayButton: React.FC<ReplayButtonProps> = ({ onReset }) => {
   // Handle button click to toggle the replay state
-  return <button onClick={onReset}>Replay?</button>;
+  return (
+    <div className="replay-button-container">
+      <button className="replay-button" onClick={onReset}>
+        <span>Replay?</span>
+      </button>
+    </div>
+  );
 };
