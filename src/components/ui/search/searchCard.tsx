07a4256c603e26ccec2f80e07d71b0fa06ccc57a
AdamS <surzyn.adam@gmail.com>
2024-12-02 19:16:42 +0100

    Dev: moving font to index.html

--- src/components/ui/search/searchCard.tsx
+++ src/components/ui/search/searchCard.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { CardCommonAttributes } from "../../../types/searchTypes";
-interface SearchCardProps {
+interface Props {
   cardData: CardCommonAttributes;
   handleCurrentCard: (cardData: CardCommonAttributes) => void;
 }
 
-export const SearchCard: React.FC<SearchCardProps> = ({
-  cardData,
-  handleCurrentCard,
-}) => {
+export const SearchCard = ({ cardData, handleCurrentCard }: Props) => {
   const setCurrentChosenCard = () => {
     handleCurrentCard(cardData);
   };
