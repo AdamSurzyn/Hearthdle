0f7d5e95d44a28ec4ed6380be8a6dfe4a3e04971
AdamS <surzyn.adam@gmail.com>
2025-01-12 14:11:40 +0100

    Dev: grid styling

--- src/components/ui/grid.tsx
+++ src/components/ui/grid.tsx
@@ -21,17 +21,28 @@ const Grid = ({ cardsComparisonArr }: { cardsComparisonArr: UserGuessArr }) => {
   //! I will want to check if the game has ended after everything here is already rendered.
   return (
     <div className="cards-grid-container">
+      {cardsComparisonArr.length > 0 && (
+        <div className="answer-titles">
+          <div className="title">Class</div>
+          <div className="title">Mana Cost</div>
+          <div className="title">Set</div>
+          <div className="title">Type</div>
+        </div>
+      )}
       {cardsComparisonArr.map((card, id) => (
         <div className="card-comparison" key={id}>
           <div className={getCardClassName(card.classCorrect)}>
             {card.className}
           </div>
+
           <div className={getManaIndicator(card.manaCostCorrect)}>
             {card.manaCost}
           </div>
+
           <div className={getCardClassName(card.setCorrect)}>
             {card.cardSet}
           </div>
+
           <div className={getCardClassName(card.typeCorrect)}>
             {card.cardType}
           </div>
